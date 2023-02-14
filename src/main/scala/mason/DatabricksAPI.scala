43d3f2243f738038ce3189974637ff84bbd380ae
Brian Tracey <brian@grenadilla-studios.com>
2023-02-14 18:20:00 -0500

    add task which allows uploading of arbitrary artifacts to Databricks workspace API

--- src/main/scala/mason/DatabricksAPI.scala
+++ src/main/scala/mason/DatabricksAPI.scala
@@ -38,6 +38,18 @@ class DatabricksAPI(
   private val apiEndpoint = (path: String) =>
     databricksHost.map(hostname => s"$hostname/api/2.0/$path")
 
+  /**
+   * Method for building HTTP requests for the Databricks API.
+   *
+   * @tparam T
+   *   typed representation of what JSON the endpoint will return
+   * @param method
+   *   http method for the request
+   * @param path
+   *   api path to build the request for
+   * @param query
+   *   query string segments to add to the request if needed
+   */
   def buildRequest[T: Decoder](
       method: Method,
       path: String,
@@ -55,6 +67,12 @@ class DatabricksAPI(
       .response(asJson[T])
   }
 
+  /**
+   * Build an HTTP request for the status of a specific cluster.
+   *
+   * @param clusterId
+   *   id of cluster for which information is being requested
+   */
   def libraryStatusRequest(clusterId: String): RequestT[Identity, Either[
     ResponseException[String, io.circe.Error],
     ClusterLibrariesStatus
@@ -71,6 +89,15 @@ class DatabricksAPI(
         )
     }
 
+  /**
+   * Use the provided library name and cluster ID to search the names of the libraries installed on
+   * the cluster for matches names and return a list of matches.
+   *
+   * @param libraryName
+   *   name of library to perform matching against already installed libraries
+   * @param clusterId
+   *   id of cluster to search for matching libraries on
+   */
   def librariesToRemove(
       libraryName: String,
       clusterId: String
@@ -93,6 +120,15 @@ class DatabricksAPI(
           .filter(_.nonEmpty)
     }
   }
+
+  /**
+   * Generates a JSON string which defines a library uninstall operation for the provided cluster.
+   *
+   * @param librariesToRemove
+   *   names of the libraries on the cluster which need to be removed
+   * @param clusterId
+   *   id of cluster to uninstall libraries from
+   */
   def uninstallJson(
       librariesToRemove: Option[Seq[String]],
       clusterId: String
@@ -106,6 +142,12 @@ class DatabricksAPI(
         |  ]
         |}""".stripMargin
 
+  /**
+   * Uninstall libraries from a cluster based on the output of uninstallJson() call
+   *
+   * @param uninstallJson
+   *   json blob which defines the libraries to uninstall and the cluster to affect
+   */
   def removeOldVersions(uninstallJson: Option[String]): Either[String, String] =
     uninstallJson match {
       case None =>
@@ -131,9 +173,18 @@ class DatabricksAPI(
         }
     }
 
-  def uploadJar(localPath: Path): Option[String] = {
-    val jarName    = localPath.getFileName.toString
-    val targetPath = s"/FileStore/jars/${jarName}"
+  /**
+   * Upload an artifact from local storage to Databricks FileStore
+   *
+   * @param localPath
+   *   path to artifact on local filesystem
+   * @param targetDir
+   *   path in Databricks FileStore to upload the artifact to
+   */
+  def uploadJar(localPath: Path, targetDir: String): Option[String] = {
+    val jarName = localPath.getFileName.toString
+    // val targetPath = s"/FileStore/jars/${jarName}"
+    val targetPath = Path.of(targetDir).resolve(jarName).toString
     buildRequest[JsonObject](Method.POST, "dbfs/put", Map.empty) match {
       case None =>
         logger.error("Unable to build request to upload new JAR")
@@ -157,6 +208,14 @@ class DatabricksAPI(
     }
   }
 
+  /**
+   * Install a library from Databricks FileStore onto a cluster.
+   *
+   * @param clusterId
+   *   id of cluster on which to install library
+   * @param libraryPath
+   *   path in Databricks FS where the library to install is located
+   */
   def installJar(clusterId: String, libraryPath: String): Boolean = {
     val installJson = s"""|{
       |  "cluster_id": "$clusterId",
@@ -189,6 +248,12 @@ class DatabricksAPI(
     }
   }
 
+  /**
+   * Send the restart command to a cluster.
+   *
+   * @param clusterId
+   *   identifier of the cluster to be restarted
+   */
   def restartCluster(clusterId: String): Boolean = {
     buildRequest[JsonObject](
       Method.POST,
@@ -220,4 +285,51 @@ class DatabricksAPI(
     }
   }
 
+  /**
+   * Upload an arbitrary scala/python/sql/r notebook file to Databricks workspace.
+   *
+   * @param sourceFilePath
+   *   the path to the notebook file on the local filesystem
+   * @param destinationFilePath
+   *   path in Databricks workspace to which the notebook should be uploaded
+   * @param overwite
+   *   whether or not to overwrite the notebook content in Databricks if the file already exists
+   */
+  def uploadFile(sourceFilePath: Path, destinationFilePath: String, overwrite: Boolean): Boolean = {
+    def detectLang(filename: String): String = {
+      val extension = filename.split("\\.").last
+      extension.toLowerCase match {
+        case "sc" | "scala" => "SCALA"
+        case "py"           => "PYTHON"
+        case "sql"          => "SQL"
+        case "r"            => "R"
+      }
+    }
+    buildRequest[JsonObject](Method.POST, "workspace/import", Map.empty) match {
+      case None =>
+        logger.error("Unable to build request to upload file artifact.")
+        false
+      case Some(req) => {
+        req
+          .multipartBody(
+            multipartFile("content", sourceFilePath),
+            multipart("path", destinationFilePath),
+            multipart("overwrite", if (overwrite) "true" else "false"),
+            multipart("format", "SOURCE"),
+            multipart("language", detectLang(sourceFilePath.getFileName.toString))
+          )
+          .send(backend)
+          .is200 match {
+          case true =>
+            logger.success(
+              s"Uploaded file artifact ${sourceFilePath.toString} to ${destinationFilePath}"
+            )
+            true
+          case false =>
+            logger.error(s"Failed to upload file artifact ${sourceFilePath.toString}")
+            false
+        }
+      }
+    }
+  }
 }
